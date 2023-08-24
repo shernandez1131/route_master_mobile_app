0426f3ed6095b37510985202633066d674d0755f
Gabriel Vasquez <gabriel.vasquez@inventiva-solutions.com>
2023-08-23 22:49:30 -0500

    fix: add main function

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'sign_in_view.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SignInView(), // Change this line to use SignInView
+      home: SignInView(),
     );
   }
+}
+
+void main() {
+  runApp(MyApp());
 }
