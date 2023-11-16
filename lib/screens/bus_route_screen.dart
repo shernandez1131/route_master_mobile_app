72faa105c9034685c29ff011e7a6742bcdf75e9a
Gabriel Vasquez <66705275+gabo197@users.noreply.github.com>
2023-11-16 08:18:23 -0500

    fix: bus line details on bus stop

--- lib/screens/bus_route_screen.dart
+++ lib/screens/bus_route_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:route_master_mobile_app/extensions.dart';
 import 'package:route_master_mobile_app/models/bus_line_model.dart';
 import 'package:route_master_mobile_app/models/bus_stop_model.dart';
 import 'package:route_master_mobile_app/services/bus_line_service.dart';
@@ -18,6 +21,7 @@ class _BusRouteScreenState extends State<BusRouteScreen> {
   Set<Marker> _markers = {};
   BitmapDescriptor? busIcon;
   List<BusLine> busLines = [];
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -47,9 +51,14 @@ class _BusRouteScreenState extends State<BusRouteScreen> {
           onTap: () async {
             print('StopId: ${busStop.busStopId}. Clicked ${busStop.name}');
             // Handle marker tap: show bus stop details, navigate, etc.
+            setState(() {
+              isLoading = true;
+            });
             busLines =
                 await BusLineService.getBusLinesByStopId(busStop.busStopId);
-
+            setState(() {
+              isLoading = false;
+            });
             //Show a dialog with the bus lines
             if (context.mounted) {
               showDialog(
@@ -63,7 +72,63 @@ class _BusRouteScreenState extends State<BusRouteScreen> {
                         shrinkWrap: true,
                         itemCount: busLines.length,
                         itemBuilder: (context, index) {
-                          return ListTile(title: Text(busLines[index].code));
+                          return ListTile(
+                            title: Row(
+                              children: [
+                                busLines[index].logo == ""
+                                    ? Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                            border: Border.all(
+                                              color:
+                                                  Colors.grey.withOpacity(0.5),
+                                              width: 1,
+                                            ),
+                                            color: Colors.white,
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: busLines[index]
+                                                    .color
+                                                    .toColor(),
+                                                spreadRadius: 0,
+                                                blurRadius: 0,
+                                                offset: const Offset(5, 0),
+                                              ),
+                                            ]),
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 4.0,
+                                              right: 4.0,
+                                              top: 2.0,
+                                              bottom: 2.0),
+                                          child: Row(
+                                            children: [
+                                              const Icon(Icons.directions_bus),
+                                              Text(
+                                                busLines[index].code,
+                                                style: const TextStyle(
+                                                    fontSize: 15),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                    : Image.memory(
+                                        base64Decode(busLines[index].logo!),
+                                        width: 36,
+                                        height: 36,
+                                        scale: 1.5,
+                                      ),
+                                const SizedBox(width: 16),
+                                Expanded(
+                                  child: Text(busLines[index].alias == null
+                                      ? '${busLines[index].firstStop} - ${busLines[index].lastStop}'
+                                      : '${busLines[index].firstStop} - ${busLines[index].lastStop} [${busLines[index].alias!}]'),
+                                )
+                              ],
+                            ),
+                          );
                         },
                       ),
                     ),
@@ -112,16 +177,29 @@ class _BusRouteScreenState extends State<BusRouteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: widget.busStops.isNotEmpty
-              ? LatLng(widget.busStops.first.latitude,
-                  widget.busStops.first.longitude)
-              : LatLng(-12.0461513,
-                  -77.0306332), // Default to center if no bus stops found
-          zoom: 12,
-        ),
-        markers: _markers,
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: widget.busStops.isNotEmpty
+                  ? LatLng(widget.busStops.first.latitude,
+                      widget.busStops.first.longitude)
+                  : LatLng(-12.0461513,
+                      -77.0306332), // Default to center if no bus stops found
+              zoom: 12,
+            ),
+            markers: _markers,
+          ),
+          isLoading
+              ? Container(
+                  color: Colors.black
+                      .withOpacity(0.5), // Semi-transparent background
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ],
       ),
     );
   }
